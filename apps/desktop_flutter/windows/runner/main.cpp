a75e16f44b1e535fe54cad8364848ab084a86df0
mastergkdigitals <mastergk.digitals@gmail.com>
2026-07-23 10:54:43 +0300

    fix: use portable Windows executable title

--- apps/desktop_flutter/windows/runner/main.cpp
+++ apps/desktop_flutter/windows/runner/main.cpp
@@ -21,7 +21,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"المميز ERP", origin, size)) {
+  if (!window.Create(L"Almumayaz ERP", origin, size)) {
     return EXIT_FAILURE;
   }
 
