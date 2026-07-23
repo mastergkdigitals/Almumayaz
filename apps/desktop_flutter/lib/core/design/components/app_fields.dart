6be5bf79faaec11733c7c7b6a7c17bfac4737366
mastergkdigitals <mastergk.digitals@gmail.com>
2026-07-23 13:53:23 +0300

    feat: add shared application fields

---
+++ apps/desktop_flutter/lib/core/design/components/app_fields.dart
@@ -0,0 +1,264 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../app_tokens.dart';
+
+class AppTextField extends StatelessWidget {
+  const AppTextField({
+    required this.controller,
+    required this.label,
+    super.key,
+    this.fieldKey,
+    this.hint,
+    this.icon,
+    this.suffixIcon,
+    this.focusNode,
+    this.validator,
+    this.onSubmitted,
+    this.onChanged,
+    this.textInputAction,
+    this.keyboardType,
+    this.inputFormatters,
+    this.textDirection,
+    this.enabled = true,
+    this.autofocus = false,
+    this.obscureText = false,
+    this.maxLines = 1,
+  });
+
+  final TextEditingController controller;
+  final String label;
+  final Key? fieldKey;
+  final String? hint;
+  final IconData? icon;
+  final Widget? suffixIcon;
+  final FocusNode? focusNode;
+  final String? Function(String?)? validator;
+  final ValueChanged<String>? onSubmitted;
+  final ValueChanged<String>? onChanged;
+  final TextInputAction? textInputAction;
+  final TextInputType? keyboardType;
+  final List<TextInputFormatter>? inputFormatters;
+  final TextDirection? textDirection;
+  final bool enabled;
+  final bool autofocus;
+  final bool obscureText;
+  final int maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      key: fieldKey,
+      controller: controller,
+      focusNode: focusNode,
+      enabled: enabled,
+      autofocus: autofocus,
+      obscureText: obscureText,
+      maxLines: obscureText ? 1 : maxLines,
+      style: AppTypography.fieldText,
+      textDirection: textDirection,
+      textInputAction: textInputAction,
+      keyboardType: keyboardType,
+      inputFormatters: inputFormatters,
+      validator: validator,
+      onFieldSubmitted: onSubmitted,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        prefixIcon: icon == null ? null : Icon(icon),
+        suffixIcon: suffixIcon,
+      ),
+    );
+  }
+}
+
+class AppIntegerField extends StatelessWidget {
+  const AppIntegerField({
+    required this.controller,
+    required this.label,
+    super.key,
+    this.fieldKey,
+    this.icon,
+    this.validator,
+    this.onSubmitted,
+    this.enabled = true,
+  });
+
+  final TextEditingController controller;
+  final String label;
+  final Key? fieldKey;
+  final IconData? icon;
+  final String? Function(String?)? validator;
+  final ValueChanged<String>? onSubmitted;
+  final bool enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppTextField(
+      controller: controller,
+      label: label,
+      fieldKey: fieldKey,
+      icon: icon,
+      enabled: enabled,
+      validator: validator,
+      onSubmitted: onSubmitted,
+      textDirection: TextDirection.ltr,
+      textInputAction: TextInputAction.next,
+      keyboardType: TextInputType.number,
+      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+    );
+  }
+}
+
+class AppMoneyField extends StatelessWidget {
+  const AppMoneyField({
+    required this.controller,
+    required this.label,
+    super.key,
+    this.fieldKey,
+    this.icon = Icons.payments_rounded,
+    this.validator,
+    this.onSubmitted,
+    this.enabled = true,
+  });
+
+  final TextEditingController controller;
+  final String label;
+  final Key? fieldKey;
+  final IconData icon;
+  final String? Function(String?)? validator;
+  final ValueChanged<String>? onSubmitted;
+  final bool enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppTextField(
+      controller: controller,
+      label: label,
+      fieldKey: fieldKey,
+      icon: icon,
+      enabled: enabled,
+      validator: validator,
+      onSubmitted: onSubmitted,
+      textDirection: TextDirection.ltr,
+      textInputAction: TextInputAction.next,
+      keyboardType: const TextInputType.numberWithOptions(decimal: true),
+      inputFormatters: [
+        FilteringTextInputFormatter.allow(
+          RegExp(r'^\d*(?:\.\d{0,4})?\$'),
+        ),
+      ],
+    );
+  }
+}
+
+class AppDropdownOption<T> {
+  const AppDropdownOption({required this.value, required this.label});
+
+  final T value;
+  final String label;
+}
+
+class AppDropdownField<T> extends StatelessWidget {
+  const AppDropdownField({
+    required this.label,
+    required this.options,
+    required this.value,
+    required this.onChanged,
+    super.key,
+    this.fieldKey,
+    this.icon,
+    this.enabled = true,
+  });
+
+  final String label;
+  final List<AppDropdownOption<T>> options;
+  final T? value;
+  final ValueChanged<T?> onChanged;
+  final Key? fieldKey;
+  final IconData? icon;
+  final bool enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField<T>(
+      key: fieldKey,
+      initialValue: value,
+      isExpanded: true,
+      style: AppTypography.fieldText,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: icon == null ? null : Icon(icon),
+      ),
+      items: options
+          .map(
+            (option) => DropdownMenuItem<T>(
+              value: option.value,
+              child: Text(option.label),
+            ),
+          )
+          .toList(growable: false),
+      onChanged: enabled ? onChanged : null,
+    );
+  }
+}
+
+class AppSwitchField extends StatelessWidget {
+  const AppSwitchField({
+    required this.title,
+    required this.value,
+    required this.onChanged,
+    super.key,
+    this.subtitle,
+    this.icon,
+  });
+
+  final String title;
+  final String? subtitle;
+  final bool value;
+  final ValueChanged<bool>? onChanged;
+  final IconData? icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      minHeight: AppControlHeights.large,
+      padding: const EdgeInsets.symmetric(
+        horizontal: AppSpacing.md,
+        vertical: AppSpacing.sm,
+      ),
+      decoration: BoxDecoration(
+        color: AppColors.surface,
+        borderRadius: BorderRadius.circular(AppRadii.md),
+        border: Border.all(color: AppColors.border),
+      ),
+      child: Row(
+        children: [
+          if (icon != null) ...[
+            Icon(icon, color: AppColors.primary),
+            const SizedBox(width: AppSpacing.md),
+          ],
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(title, style: AppTypography.fieldText),
+                if (subtitle != null)
+                  Text(
+                    subtitle!,
+                    style: const TextStyle(
+                      color: AppColors.textSecondary,
+                      fontSize: 13,
+                    ),
+                  ),
+              ],
+            ),
+          ),
+          Switch(value: value, onChanged: onChanged),
+        ],
+      ),
+    );
+  }
+}
