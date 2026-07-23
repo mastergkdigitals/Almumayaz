5e76d046f382491be9a39bcd9d63338b3dbfbaf7
mastergkdigitals <mastergk.digitals@gmail.com>
2026-07-23 12:40:04 +0300

    feat: support wrapperless application logo

--- apps/desktop_flutter/lib/core/design/app_logo.dart
+++ apps/desktop_flutter/lib/core/design/app_logo.dart
@@ -7,13 +7,31 @@ class AppLogo extends StatelessWidget {
     super.key,
     this.size = 48,
     this.padding = 6,
+    this.showBackground = true,
   });
 
   final double size;
   final double padding;
+  final bool showBackground;
 
   @override
   Widget build(BuildContext context) {
+    final image = Image.asset(
+      'assets/png/logo.png',
+      fit: BoxFit.contain,
+      errorBuilder: (_, _, _) => const Icon(
+        Icons.auto_graph_rounded,
+        color: AppColors.primary,
+      ),
+    );
+
+    if (!showBackground) {
+      return SizedBox.square(
+        dimension: size,
+        child: image,
+      );
+    }
+
     return Container(
       width: size,
       height: size,
@@ -22,14 +40,7 @@ class AppLogo extends StatelessWidget {
         color: Colors.white,
         borderRadius: BorderRadius.circular(AppRadii.md),
       ),
-      child: Image.asset(
-        'assets/png/logo.png',
-        fit: BoxFit.contain,
-        errorBuilder: (_, _, _) => const Icon(
-          Icons.auto_graph_rounded,
-          color: AppColors.primary,
-        ),
-      ),
+      child: image,
     );
   }
 }
