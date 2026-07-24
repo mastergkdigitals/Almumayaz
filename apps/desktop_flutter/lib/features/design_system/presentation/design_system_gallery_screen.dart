0e3d7b41f5442f41af6764649cae76781014f022
mastergkdigitals <mastergk.digitals@gmail.com>
2026-07-24 18:28:22 +0300

    feat: document ERP business design patterns

--- apps/desktop_flutter/lib/features/design_system/presentation/design_system_gallery_screen.dart
+++ apps/desktop_flutter/lib/features/design_system/presentation/design_system_gallery_screen.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 import '../../../core/design/app_design_system.dart';
 import 'widgets/design_gallery_actions.dart';
 import 'widgets/design_gallery_colors.dart';
+import 'widgets/design_gallery_dialogs.dart';
 import 'widgets/design_gallery_feedback.dart';
 import 'widgets/design_gallery_fields.dart';
+import 'widgets/design_gallery_formats.dart';
+import 'widgets/design_gallery_invoice_tables.dart';
 import 'widgets/design_gallery_keyboard.dart';
 
 class DesignSystemGalleryScreen extends StatelessWidget {
@@ -27,6 +30,12 @@ class DesignSystemGalleryScreen extends StatelessWidget {
             DesignGalleryActionsGroup(),
             SizedBox(height: AppSpacing.lg),
             DesignGalleryFieldsGroup(),
+            SizedBox(height: AppSpacing.lg),
+            DesignGalleryFormatsSection(),
+            SizedBox(height: AppSpacing.lg),
+            DesignGalleryBusinessDialogsSection(),
+            SizedBox(height: AppSpacing.lg),
+            DesignGalleryInvoiceTablesSection(),
             SizedBox(height: AppSpacing.lg),
             DesignGalleryKeyboardSection(),
             SizedBox(height: AppSpacing.lg),
